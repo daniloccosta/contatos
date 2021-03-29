3b3cf84dfce2b3812b9a7dfb80a2a5c6f87a5c36
daniloccosta <danilocc.sistemas@gmail.com>
2021-03-29 07:51:04 -0300

    inclusão do delete na lista de contatos

--- core/models.py
+++ core/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class Contato(models.Model):
     nome = models.CharField(max_length=80)
-    whatsapp = models.CharField(max_length=20)
-    twitter = models.CharField(max_length=120)
-    facebook = models.CharField(max_length=120)
-    instagram = models.CharField(max_length=120)
-    linkedin = models.CharField(max_length=120)
-    tiktok = models.CharField(max_length=120)
+    whatsapp = models.CharField(max_length=20, blank=True)
+    twitter = models.CharField(max_length=120, blank=True)
+    facebook = models.CharField(max_length=120, blank=True)
+    instagram = models.CharField(max_length=120, blank=True)
+    linkedin = models.CharField(max_length=120, blank=True)
+    tiktok = models.CharField(max_length=120, blank=True)
 
     class Meta:
         db_table = 'contatos'
